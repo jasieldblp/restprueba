86b2f197e4a38d3422a4b21bf5d6478057a88e02
jasieldblp <2236000932@utna.edu.mx>
2025-06-19 17:11:11 -0600

    se realiza la conexcion a la base de datos de mongodb

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const CONFIG = require('./app/config/configuracion')
 const app = require('./app/app')
+const conexion = require('./app/config/conexion')
+
+conexion.connect()
 
 app.listen(CONFIG.PORT,()=>{
     console.log(`server is running on port ${CONFIG.PORT}`);
